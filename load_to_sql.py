1b4542b945e464d2c75e7ffe6f5ea94102309b52
GuanqianWang <79542560+GuanqianWang@users.noreply.github.com>
2024-11-03 20:16:11 -0500

    Update load_to_sql.py

--- load_to_sql.py
+++ load_to_sql.py
@@ -4,17 +4,17 @@ from sqlalchemy import create_engine
 df = pd.read_csv('Your_dataset.csv')
 
 # Define your connection parameters
-username = 'postgres' #this is a typical name for server
+username = 'postgres' # This is the default user name, use your username if defined otherwise
 password = 'your password'
 host = 'localhost'  # or your host
 port = '5432'       # default port for PostgreSQL
-database = 'fake_job_posting'
+database = 'your database name'
 
 # Create a connection string
 connection_string = f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}'
 engine = create_engine(connection_string)
 
 # Load the DataFrame into PostgreSQL
-df.to_sql('US_only', con=engine, if_exists='replace', index=False)
+df.to_sql('Your data frame name', con=engine, if_exists='replace', index=False)
 
 engine.dispose()
